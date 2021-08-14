cb62903962b4ecb27f83a46db0ccb2c21325bc88
Xiaotian Wu <wuxiaotian@loongson.cn>
2021-08-14 16:38:51 +0800

    fix typo

--- ld/emulparams/elf64loongarch.sh
+++ ld/emulparams/elf64loongarch.sh
@@ -1,8 +1,8 @@
-. ${srcdir}/emulparams/elf64loongarch-defs.sh
+source_sh ${srcdir}/emulparams/elf64loongarch-defs.sh
 OUTPUT_FORMAT="elf64-loongarch"
 
 case "$target" in
-  loong64*-linux*)
+  loongarch64*-linux*)
     case "$EMULATION_NAME" in
       *64*)
 	LIBPATH_SUFFIX="64/lib64 64";;
